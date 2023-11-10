9d4e8a78ddba8303a282bce49bb1eb15cc5fdec4
b2bdokter <42370035+b2bdokter@users.noreply.github.com>
2023-11-10 18:32:37 +0000

    feat: UI update

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,18 +8,18 @@ require_once 'models/Pawn.php';
 
 <html>
     <head>
-        <link rel="stylesheet" href="styles.css">
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
         <title>Chessboard</title>
     </head>
-    <body>
+    <body class="flex flex-col items-center justify-center h-screen bg-gray-100">
 
-        <h1 class="text-xl bold text-center pt-12 mt-12">Chessboard</h1>
+        <h1 class="text-xl font-bold my-12">Chessboard</h1>
 
         <div id="chessboard" class="chessboard">
             <!-- Chessboard squares will be dynamically generated here -->
         </div>
-        <button id="startButton">Start Game</button>
+        
+        <button id="startButton" class="bg-green-500 text-white px-4 py-2 rounded mt-4">Start Game</button>
 
         <?php
         // Initialize the board
