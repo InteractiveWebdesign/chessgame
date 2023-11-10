803c6d879364d679f38b2e9713c85a7d3da2c602
b2bdokter <42370035+b2bdokter@users.noreply.github.com>
2023-11-10 19:53:28 +0000

    feat: added movement info to a footer

--- htdocs/index.php
+++ htdocs/index.php
@@ -16,7 +16,6 @@ require_once 'models/King.php';
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Chessboard</title>
 </head>
-<!-- <body class="flex flex-col items-center justify-center h-screen bg-gray-100 mx-auto max-w-screen-lg"> -->
 <body class="flex flex-row justify-center h-screen bg-gray-100">
 
 <!-- Player One Column -->
@@ -28,25 +27,39 @@ require_once 'models/King.php';
 
 <!-- Chessboard and Moves Column -->
 <div class="flex flex-col items-center justify-center w-3/5 p-4">
-<h1 class="text-xl font-bold my-12">Chessboard</h1>
+    <h1 class="text-xl font-bold my-12">Chessboard</h1>
 
-<div id="chessboard" class="chessboard">
-    <!-- Chessboard squares will be dynamically generated here -->
-</div>
+    <div id="chessboard" class="chessboard">
+        <!-- Chessboard squares will be dynamically generated here -->
+    </div>
 
-<button id="startButton" class="bg-green-500 text-white px-4 py-2 rounded mt-4">Start Game</button>
-<?php
-// Initialize the board
-$chessboard = new Chessboard();
-$chessboard->displayBoard();
-?>
+    <button id="startButton" class="bg-green-500 text-white px-4 py-2 rounded mt-4">Start Game</button>
 
-<!-- Move Log -->
-<div id="moveLog" class="mt-4">
+    <?php
+    // Initialize the board
+    $chessboard = new Chessboard();
+    $chessboard->displayBoard();
+    ?>
+
+    <!-- Move Log -->
+    <div id="moveLog" class="bg-black text-white flex flex-col items-center justify-center w-full p-4 rounded-t-lg">
+    <!-- <div id="moveLog" class="bg-black text-white flex flex-col items-center justify-center w-2/3 p-4"> -->
         <!-- Move log entries will be dynamically added here -->
+        <?php
+        // Fake commentary about moves (replace with actual move log)
+        $moveLogEntries = [
+            'Player One moved Pawn to E4',
+            'Player Two moved Knight to C6',
+            'Player One moved Bishop to G5',
+            // ... add more entries as needed
+        ];
+
+        foreach ($moveLogEntries as $entry) {
+            echo "<p>$entry</p>";
+        }
+        ?>
     </div>
 </div>
-
 
 <!-- Player Two Column -->
 <div class="flex flex-col items-center justify-center w-1/5 p-4">
@@ -54,9 +67,6 @@ $chessboard->displayBoard();
     <input type="text" id="playerTwoName" class="mb-2 p-2 border rounded" placeholder="Enter name">
     <!-- Additional content for player two if needed -->
 </div>
-
-
-
 
 <script>
     // Assuming the pieces are represented by Piece objects
