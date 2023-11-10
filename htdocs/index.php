48441e0b8168e3b5fe5e41efd998956c62329388
b2bdokter <42370035+b2bdokter@users.noreply.github.com>
2023-11-10 17:52:29 +0000

    feat: added tailwind css

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,8 +6,19 @@ require_once 'models/Chessboard.php';
 require_once 'models/Piece.php';
 require_once 'models/Pawn.php';
 // ... include other classes ...
+?>
 
-
-// Initialize the board
-$chessboard = new Chessboard();
-$chessboard->displayBoard();
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <title>Chessboard</title>
+    </head>
+    <body>
+        <h1 class="text-xl bold text-center pt-12 mt-12">Chessboard</h1>
+        <?php
+        // Initialize the board
+        $chessboard = new Chessboard();
+        $chessboard->displayBoard();
+        ?>
+    </body>
+</html>
--- htdocs/models/Chessboard.php
+++ htdocs/models/Chessboard.php
@@ -9,14 +9,14 @@ class Chessboard {
     }
 
     public function displayBoard() {
-        // Display the current state of the chessboard with CSS styling
-        echo '<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">';
-        echo '<table style="border-collapse: collapse; width: 80%;">';
+        // Display the current state of the chessboard with Tailwind CSS styling
+        echo '<div class="flex justify-center items-center h-screen">';
+        echo '<table class="table-auto border-collapse w-80">';
         for ($row = 0; $row < 8; $row++) {
             echo '<tr>';
             for ($col = 0; $col < 8; $col++) {
-                $color = ($row + $col) % 2 == 0 ? '#FFFFFF' : '#000000';
-                echo '<td style="width: 50px; height: 50px; background-color: ' . $color . ';"></td>';
+                $color = ($row + $col) % 2 == 0 ? 'bg-white' : 'bg-black';
+                echo '<td class="w-10 h-10 ' . $color . '"></td>';
             }
             echo '</tr>';
         }
