0ed8ac28cad6875da6ff66c8cd2010e7227db985
b2bdokter <42370035+b2bdokter@users.noreply.github.com>
2023-11-11 14:37:17 +0000

    fix: debug squares mogelijk

--- htdocs/scripts.js
+++ htdocs/scripts.js
@@ -1,21 +1,22 @@
 // Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
-    // Log that the script is loaded
     console.log('Script loaded.');
 
-    // Select all squares on the board
-    const squares = document.querySelectorAll('[data-row][data-col]');
+    // Select the board
+    const board = document.querySelector('#chessboard-container');
 
-    // Iterate over each square
-    squares.forEach(square => {
-        // Add a click event listener to each square
-        square.addEventListener('click', event => {
+    // Add a click event listener to the board
+    board.addEventListener('click', event => {
+        // Find the closest square to the event target
+        const square = event.target.closest('[data-row][data-col]');
+
+        if (square) {
             // Get the row and column of the clicked square
-            const row = event.target.getAttribute('data-row');
-            const col = event.target.getAttribute('data-col');
+            const row = square.getAttribute('data-row');
+            const col = square.getAttribute('data-col');
 
             // Log the row and column to the console
             console.log(`Clicked square at row ${row}, column ${col}`);
-        });
+        }
     });
 });
